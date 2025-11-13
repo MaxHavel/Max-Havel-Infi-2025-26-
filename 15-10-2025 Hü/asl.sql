f5b2765513fc68fd3d6bd825526e38d96c3c697b
Max Havel <HAV240107@spengergasse.at>
2025-11-13 15:21:14 +0100

    gitignore

--- 15-10-2025 Hü/asl.sql
+++
@@ -1,9 +0,0 @@
-select question from Questiion where question like 'Who %';
-
-select avg(opentb_id)from Category Where name LIKE 'E%'
-
-slect sum (finished_at) from _prisma_migrations where started_at like '%6';
-
-select B from _IncorrectAnswers where B like 'cmfvala%';
-
-select answer from answer where answer like 'Ch%';
--- 2025-09-25_sqlite_mit_deno/main.ts
+++
@@ -1,6 +0,0 @@
-import { DatabaseSync } from "node:sqlite";
-
-const db = new DatabaseSync("2ahwii.db");
-const stmt = db.prepare("SELECT * FROM students");
-const rows = stmt.all();
-console.log(rows);
